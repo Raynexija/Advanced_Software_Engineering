af225d9b82f3265e4b495e3d7ddbadfc64c1101d
TheRaynex <rafael.luedtke@web.de>
2023-05-24 14:16:53 +0200

    refactor: removed Mockito

--- src/test/java/de/dhbw/ka/test/CommandControllerTest.java
+++ src/test/java/de/dhbw/ka/test/CommandControllerTest.java
@@ -1,37 +1,43 @@
 package de.dhbw.ka.test;
 
+import de.dhbw.ka.adapter.textBased.TextBasedInputAdapter;
+import de.dhbw.ka.adapter.textBased.TextBasedOutputAdapter;
 import de.dhbw.ka.application.commands.Command;
 import de.dhbw.ka.application.CommandController;
 import de.dhbw.ka.application.interfaces.InputService;
 import de.dhbw.ka.application.interfaces.OutputService;
+import de.dhbw.ka.plugins.inputOutput.ConsoleOutputInputSystem;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Queue;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CommandControllerTest {
     private CommandController controller;
-    @Mock
-    private InputService inputService;
-    @Mock
-    private OutputService outputService;
 
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+    private ConsoleOutputInputSystem console = new ConsoleOutputInputSystem(outputStream, System.in);
+    private InputService inputService = new TextBasedInputAdapter(console);
+    private OutputService outputService = new TextBasedOutputAdapter(console);
 
 
     @BeforeEach
     void setUp() {
-        MockitoAnnotations.openMocks(this);
         controller = new CommandController(inputService, outputService);
     }
 
     @Test
     void testEnqueueCommand() {
-        Command command = Mockito.mock(Command.class);
+        Command command = new Command() {
+            @Override
+            public void execute(InputService input, OutputService output) {
+                output.displayMessage("Test");
+            }
+        };
         controller.enqueueCommand(command);
         Queue<Command> commandQueue = controller.getCommandQueue();
         assertEquals(1, commandQueue.size());
